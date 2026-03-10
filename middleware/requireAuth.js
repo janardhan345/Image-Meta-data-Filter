6f381012e483b2afb9742ddb6b5d69e2228039df
janardhan345 <janabalu694@gmail.com>
2026-03-10 19:15:22 +0530

    [docs]:comments

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -4,7 +4,7 @@ const requireAuth = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if(!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({
+        return res.status(401).json({ // 401 unauthorized 
             success:false,
             error: 'No token provided'
         });

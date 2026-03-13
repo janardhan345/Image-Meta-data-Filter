c8c22e57c554bab57ed59bc9076b60c4089a4822
janardhan345 <janabalu694@gmail.com>
2026-03-13 17:24:58 +0530

    [refactor] text

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
             type:String,
             required:[true, 'Name is required'],
             trim: true,
-            minlength:[2, 'Name must be at least characters']
+            minlength:[2, 'Name must be at least 2 characters']
         },
         email:{
             type:String,
